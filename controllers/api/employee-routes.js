3bbc0bc2abe2ffdfdb6929bbbf2f5fd5c988c8dc
Lawrence Rivales <lrivales@gmail.com>
2021-11-23 10:32:04 -0500

    added employee routes

---
+++ controllers/api/employee-routes.js
@@ -0,0 +1,99 @@
+const router = require('express').Router();
+const { Customer, Employee, Menu_Category, Menu_Item, Order_Item, Order } = require('../../models');
+
+// get all employees
+router.get('/', (req, res) => {
+    Employee.findAll()
+    .then(dbEmployeeData => res.json(dbEmployeeData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// get employee by id
+router.get('/:id', (req, res) => {
+    Employee.findAll(
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    .then(dbEmployeeData => {
+        if (!dbEmployeeData) {
+            res.status(404).json({ message: 'Employee ID not found.'});
+            return;
+        }
+        res.json(dbEmployeeData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// add employee
+router.post('/', (req, res) => {
+    Employee.create(
+        {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+        }
+    )
+    .then(dbEmployeeData => res.json(dbEmployeeData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// update employee
+router.put('/:id', (req, res) => {
+    Employee.update(
+        {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    .then(dbEmployeeData => {
+        if (!dbEmployeeData) {
+            res.status(404).json({ message: 'Employee ID not found.'});
+            return;
+        }
+        res.json(dbEmployeeData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// delete employee
+router.delete('/:id', (req, res) => {
+    Employee.destroy(
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    .then(dbEmployeeData => {
+        if (!dbEmployeeData) {
+            res.status(404).json({ message: 'Employee ID not found.'});
+            return;
+        }
+        res.json(dbEmployeeData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const customerRoutes = require('./customer-routes');
+const employeeRoutes = require('./employee-routes')
 
 router.use('/customers', customerRoutes);
+router.use('/employees', employeeRoutes);
 
 module.exports = router;
