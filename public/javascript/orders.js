678831aeb7f79f4a3d55b21bdcb9bf093230bdd3
Nadine <nadine_paez@hotmail.com>
2021-11-30 16:17:00 -0500

    fix commit

--- public/javascript/orders.js
+++ public/javascript/orders.js
@@ -1,8 +1,10 @@
 // populate orders from sequelize
 
 //add functionality to 'order finished' button -> order card disappears when button is clicked
+async function deleteCardHandler(event) {
+    event.preventDefault();
+    
+};
 
-
-
-
+document.querySelector('.delete').addEventListener('click', deleteCardHandler);
 
