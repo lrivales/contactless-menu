7b0c72765efc626b35a0bf595cae84facfe666c7
Snake <chaldrich24@gmail.com>
2021-12-01 18:01:54 -0500

    add edit quantity functionality to cart

--- public/javascript/cart.js
+++ public/javascript/cart.js
@@ -1,12 +1,48 @@
 const editButton = document.querySelectorAll('.edit-cart-btn');
 const deleteButton = document.querySelectorAll('.delete-cart-btn');
 
-function editItem() {
+function editItem(event) {
+    const order_id = localStorage.getItem('orderId');
+    const menu_item_id = event.path[2].id;
+    const quantityPEl = event.target.parentElement.children[0];
 
+    const quantityInput = document.createElement('select');
+    quantityInput.type = 'select';
+
+    for (i=1; i < 6; i++) {
+        const quantityOption = document.createElement('option');
+        quantityOption.innerHTML = i;
+        quantityInput.appendChild(quantityOption);
+    }
+
+    quantityInput.className = 'cart-quantity-input';
+    quantityPEl.replaceWith(quantityInput);
+    event.target.innerHTML = 'Confirm';
+
+    event.target.addEventListener('click', () => {
+        const newQuantity = quantityInput.selectedIndex + 1;
+        confirmEdit(order_id, menu_item_id, newQuantity)
+    });
 }
 
 function deleteItem() {
 
+}
+
+async function confirmEdit(order_id, menu_item_id, quantity) {
+    const response = await fetch(`/api/order_items/${order_id}/${menu_item_id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            quantity
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace(`/cart/${order_id}`);
+    }
 }
 
 editButton.forEach(btn => btn.addEventListener('click', editItem));
